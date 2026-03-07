cdf7b242c2524b15fead1bec68b9a2a560ec6202
Emin_Ocak11 <eminocak85@gmail.com>
2026-03-08 01:27:34 +0300

    Add Project Structure Image

--- App/main.py
+++ App/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 # Creates FastAPI object 
-app = FastAPI()
+app = FastAPI(
+    title="Super App",
+    description="We download apps for our needs but why we download different app for different needs. I decided fix this problem.",
+    version="0.01"
+)
 
 # Creates Healthy API
 @app.get("/", tags=['Healthy'])
@@ -11,7 +15,10 @@ async def healthy_check():
                 return (dict): status | messages
                 return_type (dict): Python dict.
     """
-    return {
-        "status": True,
-        "messages": "Application is healthy."
-    }
+    try:
+        return {
+            "status": True,
+            "messages": "Application is healthy."
+        }
+    except:
+        HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong somethink...")
